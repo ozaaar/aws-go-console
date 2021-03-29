525d2789d9f35d6c3d34167836d0d9e4606d5ebd
Qasim Sarfraz <qasim.sarfraz@esailors.de>
2021-03-29 12:23:34 +0000

    Add API

---
+++ console.go
@@ -0,0 +1,31 @@
+package console
+
+import (
+	"net/url"
+	"time"
+)
+
+// Token contains the sign-in token for AWS console access
+// The Value is passed with in the url at the time of accessing
+type Token struct {
+	Value     string    `json:"SigninToken"`
+	ExpiresAt time.Time `json:"-"`
+}
+
+// SignInToken fetches the token from AWS API via GetFederationToken
+func SignInToken() (*Token, error) {
+	panic("not implemented")
+}
+
+// IsValid confirms the validity for a given token
+func (t *Token) IsValid() bool {
+	if t.Value != "" && !t.ExpiresAt.IsZero() && t.ExpiresAt.After(time.Now()) {
+		return true
+	}
+	return false
+}
+
+// SignInURL returns the URL with token included for short time access
+func (t *Token) SignInURL() url.URL {
+	panic("not implemented")
+}
---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/ozaaar/aws-go-console
+
+go 1.16
